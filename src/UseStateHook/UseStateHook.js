8ced96dbced9fe527134b7b8f1c203f6e0ec4eb2
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-10 22:41:29 +0300

    1. Adds UseStateHook

--- src/UseStateHook/UseStateHook.js
+++ src/UseStateHook/UseStateHook.js
@@ -22,4 +22,29 @@ import React, { useState } from "react";
     https://reactjs.org/docs/hooks-reference.html#usereducer
 */
 
-export const Form = () => {};
+export const Form = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        if (email !== '' && password !== '') {
+            setMessage('Вы вошли');
+        }
+        else {
+            setMessage('');
+        }
+    }
+
+    return (
+        <div>
+            <input type="email" name="email" data-testid="email-input" value={email} onChange={ (e) => setEmail(e.target.value) } />
+            <input type="password" name="password" data-testid="password-input" value={password} onChange={ (e) => setPassword(e.target.value) } />
+            <input type="submit" name="submit" data-testid="submit" onClick={ handleSubmit } />
+
+            {message && <div data-testid="success-message">{message}</div>}
+        </div>
+    );
+};
